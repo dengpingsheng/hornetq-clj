9eeca30601eca683975d65d90948d94fa99e96cc
zzwu <greatzzwu@gmail.com>
2013-11-18 13:57:33 +0800

    project

--- client/project.clj
+++ client/project.clj
@@ -1,6 +1,7 @@
 (defproject hornetq-clj/client "0.2.2-SNAPSHOP"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [org.hornetq/hornetq-core-client "2.3.0.Final"]]
+                 [org.hornetq/hornetq-core-client "2.2.21.Final"]
+                 [org.jboss.netty/netty "3.2.1.Final"]]
   :repositories {"JBoss releases"
                  "http://repository.jboss.org/nexus/content/groups/public-jboss/"})
